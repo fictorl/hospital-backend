2760223d8ebc4bbd03b0dc63e85a5f2a6050c03e
emanuel-j-silva <emanuel.josesilva04@gmail.com>
2024-09-18 17:34:28 -0300

    feat: add rotas de exames

--- src/rotas/exames/exames.js
+++ src/rotas/exames/exames.js
@@ -1,9 +1,162 @@
 const express = require('express');
 const { SECRET_KEY } = require('../../config');
 const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
 const expressJwt = require('express-jwt');
 
 const router = express.Router();
 const prisma = new PrismaClient();
 const requireAuth = expressJwt.expressjwt({ secret: SECRET_KEY, algorithms: ['HS256'] });
+
+function isAdmin(req, res, next) {
+    if (req.auth && req.auth.role === 'admin') {
+        return next();
+    }
+    return res.status(403).json({ message: 'Acesso negado. Apenas administradores têm acesso a essa rota' });
+}
+
+router.post('/exames', requireAuth, isAdmin, async (req,res) => {
+    const { idMedico, idPaciente, resultado, dataHorario, nomeExame } = req.body;
+    try {
+        const exame = await prisma.exame.create({
+            data: { idMedico, idPaciente, resultado, dataHorario, nomeExame },
+            include: {
+                medico: {
+                    select: {
+                        nome: true,
+                        CRI: true,
+                        especialidade: true
+                    }
+                },
+                paciente: {
+                    select: {
+                        nome: true,
+                        CPF: true
+                    }
+                }
+            }
+        });
+        res.status(201).json(exame);
+    } catch (error) {
+        res.status(400).json({ message: 'Erro ao criar exame', error });
+    }
+});
+
+router.get('/exames',  requireAuth, isAdmin, async (req, res) => {
+    try {
+        const exames = prisma.exame.findMany({
+            include: {
+                medico: {
+                    select: {
+                        nome: true,
+                        CRI: true,
+                        especialidade: true
+                    }
+                },
+                paciente: {
+                    select: {
+                        nome: true,
+                        CPF: true
+                    }
+                }
+            }
+        });
+        res.status(201).json(exames);
+    } catch (error) {
+        res.status(400).json({ message: 'Erro ao procurar exames', error });        
+    }
+});
+
+router.get('/exames/:id', requireAuth, isAdmin, async (req, res) =>{
+    const { id } = req.params;
+    try {
+        const exame = prisma.exame.findUnique({
+            where: {id},
+            include: {
+                medico: {
+                    select: {
+                        nome: true,
+                        CRI: true,
+                        especialidade: true
+                    }
+                },
+                paciente: {
+                    select: {
+                        nome: true,
+                        CPF: true
+                    }
+                }
+            }         
+        })
+
+        if(!exame){
+            return res.status(404).json({message: 'Exame não encontrado'});
+        }
+
+        res.status(200).json(exame);
+    } catch (error) {
+        res.status(400).json({ message: 'Erro ao procurar exame', error });        
+    }
+});
+
+router.get('/exames/:idMedico', requireAuth, isAdmin, async (req, res) =>{
+    const { idMedico } = req.params;
+    try {
+        const exames = prisma.exame.findMany({
+            where: {idMedico},
+            include: {
+                medico: {
+                    select: {
+                        nome: true,
+                        CRI: true,
+                        especialidade: true
+                    }
+                },
+                paciente: {
+                    select: {
+                        nome: true,
+                        CPF: true
+                    }
+                }
+            }         
+        })
+
+        if(exames.length === 0){
+            return res.status(404).json({message: 'Lista de exames por médico não encontrada'});
+        }
+        res.status(200).json(exames);
+    } catch (error) {
+        res.status(400).json({ message: 'Erro ao procurar exames', error });        
+    }
+});
+
+router.get('/exames/:idPaciente', requireAuth, isAdmin, async (req, res) =>{
+    const { idPaciente } = req.params;
+    try {
+        const exames = prisma.exame.findMany({
+            where: {idPaciente},
+            include: {
+                medico: {
+                    select: {
+                        nome: true,
+                        CRI: true,
+                        especialidade: true
+                    }
+                },
+                paciente: {
+                    select: {
+                        nome: true,
+                        CPF: true
+                    }
+                }
+            }         
+        })
+
+        if(exames.length === 0){
+            return res.status(404).json({message: 'Lista de exames por paciente não encontrada'});
+        }
+
+        res.status(200).json(exames);
+    } catch (error) {
+        res.status(400).json({ message: 'Erro ao procurar exames', error });        
+    }
+});
