ac4ba2401c2d4c5381af0f05853d1d1ffbd7ab44
EDUARDO DUARTE DERISSO <115571853+EduDD125@users.noreply.github.com>
2024-11-11 20:12:03 -0300

    fix: changing CORs origin option

--- src/corsConfig.js
+++ src/corsConfig.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-    origin: "http://localhost:5000",
+    origin: "https://hospital-frontend-git-siteselenium-edudd125s-projects.vercel.app/",
     methods: "GET, POST, PUT, DELETE"
 };
 
