62f3a38d7380a74ad6073dfc1f1bc31885fc7803
EDUARDO DUARTE DERISSO <115571853+EduDD125@users.noreply.github.com>
2024-11-11 20:55:40 -0300

    Update corsConfig.js

--- src/corsConfig.js
+++ src/corsConfig.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-    origin: "https://hospital-frontend-git-siteselenium-edudd125s-projects.vercel.app/",
+    origin: "https://hospital-frontend-git-siteselenium-edudd125s-projects.vercel.app",
     methods: "GET, POST, PUT, DELETE"
 };
 
