d913bfce37c6f557fadb662b0134d46e8a74cbbf
fictorl <fictorlps@gmail.com>
2024-11-19 15:45:48 -0300

    alo

--- src/corsConfig.js
+++ src/corsConfig.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-    origin: "https://hospital-frontend-git-siteselenium-edudd125s-projects.vercel.app/",
+    origin: "https://site-test-selenium.vercel.app/",
     methods: "GET, POST, PUT, DELETE"
 };
 
